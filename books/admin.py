05c5392320546fae1e40489f8607492deb92e992
Danil Mironov <danmir@me.com>
2013-07-08 23:44:25 +0600

    Специальное отображение в Админ панели

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,11 @@
+#-*- coding: utf-8 -*-
 from django.contrib import admin
 from books.models import Publisher, Author, Book
 
+#Для отображения в admin панели
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
