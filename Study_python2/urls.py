5847233e32cc81746d15a349f0d6c1ba5a68ad09
Danil Mironov <danmir@me.com>
2013-07-08 10:42:10 +0600

    Add Project

---
+++ Study_python2/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
+from Study_python2.views import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^hello/$', hello),
+    (r'^$', myHomePageView),
+    (r'^time/plus/(\d{1,2})/$', hoursAhead),
+    # Examples:
+    # url(r'^$', 'Study_python2.views.home', name='home'),
+    # url(r'^Study_python2/', include('Study_python2.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ Study_python2/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse, Http404
+from django.template.loader import get_template
+from django.template import Context
+from django.shortcuts import render_to_response
+import datetime
+
+__author__ = 'apple'
+
+def hello(request):
+    now = datetime.datetime.now()
+    return render_to_response('current_datetime.html', {'current_date': now})
+    #html = "<html><body>Сейчас {} </body></html>".format(now) #1 версия
+    #t = get_template('current_datetime.html') #2 версия
+    #html = t.render(Context({'current_date': now})) #2 версия
+    #return HttpResponse(html) #2 версия
+
+def myHomePageView(request):
+    return HttpResponse("My Home page")
+
+def hoursAhead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours = offset)
+    #assert False
+    html = "<html><body>Через {} часов будет {}.</body></html>".format(offset, dt)
+    return HttpResponse(html)
