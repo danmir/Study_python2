6940c7559c4d9617662fb9216ba9a12899a3a7f0
Danil Mironov <danmir@me.com>
2013-07-08 23:56:27 +0600

    Панель поиска

--- books/admin.py
+++ books/admin.py
@@ -5,6 +5,7 @@ from books.models import Publisher, Author, Book
 #Для отображения в admin панели
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
 
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
