32407680b981e7499a0317c0d050ab9a52438d37
Danil Mironov <danmir@me.com>
2013-07-09 23:36:45 +0600

    Отображения модели в админ панели

--- books/admin.py
+++ books/admin.py
@@ -7,6 +7,12 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
     search_fields = ('first_name', 'last_name')
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)  #'-' В порядке убывания
+
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
