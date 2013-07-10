61d49cf21e0ab3e548ecb3a87b6ae6f5a0bde726
Danil Mironov <danmir@me.com>
2013-07-10 11:35:21 +0600

    Ошибки передаваемые в шаблон теперь список

--- books/views.py
+++ books/views.py
@@ -5,12 +5,14 @@ from books.models import Book
 # Create your views here.
 
 def search(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            error = True
+            errors.append('Введите поисковый запрос')
+        elif len(q) > 20:
+            errors.append('Введите не более 20 символов')
         else:
             books = Book.objects.filter(title__icontains = q)
             return  render_to_response('search_result.html', {'books' : books, 'query' : q})
-    return render_to_response('search_form.html', {'error' : error})
+    return render_to_response('search_form.html', {'errors' : errors})
