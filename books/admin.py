ea367a8c6b4fd198d4a2256b35360c6f84c5abd9
Danil Mironov <danmir@me.com>
2013-07-10 09:21:26 +0600

    Формы редактирования

--- books/admin.py
+++ books/admin.py
@@ -12,6 +12,9 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_date',)
     date_hierarchy = 'publication_date'
     ordering = ('-publication_date',)  #'-' В порядке убывания
+    #Порядок следования в форме редактирования
+    fields = ('title', 'authors', 'publisher', 'publication_date')
+    filter_horizontal = ('authors',)
 
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
